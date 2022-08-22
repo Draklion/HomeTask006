5658f20402c8213a885fec7cc3605b9f95df9d45
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-23 00:11:52 +0500

    Added changes.

--- HomeTask006/HomeTask006.1/Program.cs
+++ HomeTask006/HomeTask006.1/Program.cs
@@ -1,21 +1,22 @@
-﻿void CW(string msg)
+﻿try
 {
-  Console.WriteLine(msg);
-}
-string CR()
-{
-  return Console.ReadLine();
-}
+  void CW(string msg)
+  {
+    Console.WriteLine(msg);
+  }
+  string CR()
+  {
+    return Console.ReadLine();
+  }
 
-void existenceTriangle(double[] array, out string msg)
-{
-  msg = array[0] > array[1] + array[2] ||
-        array[1] > array[0] + array[2] ||
-        array[2] > array[0] + array[1] ? "Треугольник не существует" : "Треугольник существует";
-}
+  void existenceTriangle(double[] array, out string msg)
+  {
+    msg = array[0] > array[1] + array[2] ||
+          array[1] > array[0] + array[2] ||
+          array[2] > array[0] + array[1] ? "Треугольник не существует" : "Треугольник существует";
+  }
 
-try
-{
+
   double[] sides = new double[3];
   string str = "";
   for (int i = 0; i < sides.Length; i++)
