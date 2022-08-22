b4deaf7e32f198cd2acf74b33c09868d99d4c9c2
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-23 01:12:30 +0500

    HomeTask006.2 completed.

---
+++ HomeTask006/HomeTask006.2/Program.cs
@@ -0,0 +1,28 @@
+﻿void CWL(string msg)
+{
+  Console.WriteLine(msg);
+}
+void CW(string msg)
+{
+  Console.Write(msg);
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+try
+{
+  void recursion(int N)
+  {
+    if (N <= 0)
+      return;
+    recursion(N / 2);
+    CW($"{N % 2}");
+  }
+
+  CWL("Задание 2. Программа, которая будет преобразовывать десятичное число в двоичное.");
+  CWL("Введите число: ");
+  int number = Int32.Parse(CR());
+  recursion(number);
+}
+catch { CWL("Вы ввели некорректное значение."); }
