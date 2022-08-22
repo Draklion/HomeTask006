ab7989894b52d90a9fef0b4cc42c8a3c8956859f
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-23 00:10:45 +0500

    HomeTask006.1 completed.

--- HomeTask006/HomeTask006.1/Program.cs
+++ HomeTask006/HomeTask006.1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CW(string msg)
+{
+  Console.WriteLine(msg);
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+
+void existenceTriangle(double[] array, out string msg)
+{
+  msg = array[0] > array[1] + array[2] ||
+        array[1] > array[0] + array[2] ||
+        array[2] > array[0] + array[1] ? "Треугольник не существует" : "Треугольник существует";
+}
+
+try
+{
+  double[] sides = new double[3];
+  string str = "";
+  for (int i = 0; i < sides.Length; i++)
+    sides[i] = Int32.Parse(CR());
+  existenceTriangle(sides, out str);
+  CW(str);
+}
+catch { CW("Вы ввели некорректное значение."); }
