8f80707cbb828e6e3022ed52d7f069f2908e46c1
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-27 19:36:25 +0500

    HomeTask006.6 completed.

--- HomeTask006/HomeTask006.6/Program.cs
+++ HomeTask006/HomeTask006.6/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CW(string msg)
+{
+  Console.WriteLine(msg);
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+
+try
+{
+  CW("Задание 6. Программа,  которая будет создавать копию заданного массива с помощью поэлементного копирования.");
+  CW("Введите количество элементов массива: ");
+
+  int number = Int32.Parse(CR());
+  int[] sides = new int[number];
+  CW("Введите элементы массива: ");
+
+  for (int i = 0; i < number; i++) sides[i] = Int32.Parse(CR());
+
+  Сopying(sides);
+
+  void Сopying(int[] array, int i = 0)
+  {
+    int[] arrayCopy = new int[array.Length];
+    if (i < array.Length)
+    {
+      arrayCopy[i] = array[i];
+      if (i == arrayCopy.Length - 1) CW($"Скопированный массив:[{string.Join(",", array)}]");
+      Сopying(array, i + 1);
+    }
+  }
+}
+catch (InvalidCastException e) { }
