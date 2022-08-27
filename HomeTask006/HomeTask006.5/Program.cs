e15c598846ea5b8f9cc73005a45cd7388b5b0035
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-27 19:19:51 +0500

    HomeTask006.5 completed.

--- HomeTask006/HomeTask006.5/Program.cs
+++ HomeTask006/HomeTask006.5/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CW(string msg)
+{
+  Console.WriteLine(msg);
+}
+void CWR(string msg)
+{
+  Console.WriteLine(msg);
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+
+try
+{
+  CW("Задание 5. Программа, которая выводит первые N чисел Фибоначчи.");
+  CW("Введите количество чисел, которые необходимо вывести: ");
+  int number = Int32.Parse(CR());
+  int[] array = new int[number];
+
+  int firstNumberFibonacci = 0;
+  int secondNumberFibonacci = 1;
+
+  array[0] = firstNumberFibonacci;
+  array[1] = secondNumberFibonacci;
+
+  for (int i = 2; i < number; i++)
+  {
+    if (number < 2) break;
+
+    array[i] = firstNumberFibonacci + secondNumberFibonacci;
+    firstNumberFibonacci = secondNumberFibonacci;
+    secondNumberFibonacci = array[i];
+  }
+  CW(string.Join(",", array));
+}
+catch (InvalidCastException e) { }
