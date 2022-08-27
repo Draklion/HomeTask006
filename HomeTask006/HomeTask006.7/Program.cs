5d2b3ced14e3438c25ac84ec2508bad711530e98
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-27 20:10:11 +0500

    HomeTask006.7 completed.

--- HomeTask006/HomeTask006.7/Program.cs
+++ HomeTask006/HomeTask006.7/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CW(string msg)
+{
+  Console.WriteLine(msg);
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+
+try
+{
+  CW("Задание 7. Программа, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.");
+  CW("Введите число: ");
+
+  int number = Int32.Parse(CR());
+
+  Factorial(number);
+
+  void Factorial(int number, int i = 1, int composition = 1)
+  {
+    if (i <= number)
+    {
+      composition = composition * i;
+      if (i == number) CW($"Произведение чисел от 1 до {number}: {composition}");
+      Factorial(number, i + 1, composition);
+    }
+  }
+}
+catch (InvalidCastException e) { }
